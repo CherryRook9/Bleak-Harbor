0789df783fdefbbe5c1895ed307ae2270c598227
Oceiros <ivasiliev1999@gmail.com>
2019-08-06 20:52:36 +0300

    Начата разработка главного меню.

---
+++ bleak harbor/Assets/Scripts/MenuControls.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControls : MonoBehaviour
+{
+    public void PlayScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exit pressed");
+    }
+}
